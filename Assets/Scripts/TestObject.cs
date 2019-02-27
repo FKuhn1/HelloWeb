15e4b63f90bbbdd5f339d6c9f2ab455464010da8
Kuhn <fabian.kuhn@htl-salzburg.ac.at>
2019-02-27 17:32:59 +0100

    Scene Loader

--- Assets/Scripts/TestObject.cs
+++ Assets/Scripts/TestObject.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class TestObject : MonoBehaviour {
 
     string helloworld = "Hello World";
 
+    public TextMeshProUGUI Text;
+
     public void PrintHelloWorld()
     {
         Debug.Log(helloworld);
+        GetComponent<TextMeshProUGUI>().text = "Hi";
+
+        
 
     }
 
